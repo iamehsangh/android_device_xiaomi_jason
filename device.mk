88ddfaca9093044f03f2a9cbc19dca5ee698aa22
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-13 14:18:32 +0200

    jason: Move to common IR impl
    Change-Id: If984976fe0eea957a4e88a0442d8bb4dd0db01b0

--- device.mk
+++ device.mk
@@ -74,12 +74,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.dualcam.defer.enable=1 \
 
 # Consumerir
-PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl \
-    android.hardware.ir@1.0-service
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
+BOARD_HAVE_IR := true
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
