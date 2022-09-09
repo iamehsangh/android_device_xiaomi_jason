e62b240e749b7d4a6d8029a4dffb6bb569d65ab5
anrui2032 <anrui2032@gmail.com>
2022-09-09 21:39:10 +0800

    jason: Update clang ld symlink path to clang-r450784d
    Change-Id: I0649f7b6f3c619685a3b6e6d948f1ead3827a47d
    Signed-off-by: Nanhumly <admin@warbler.cc>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
 # Create ld symlink to avoid linking error(s)
-ln -s $(pwd)/prebuilts/clang/host/linux-x86/clang-r383902b/bin/lld \
-    $(pwd)/prebuilts/clang/host/linux-x86/clang-r383902b/bin/ld &> /dev/null
+ln -s $(pwd)/prebuilts/clang/host/linux-x86/clang-r450784d/bin/lld \
+    $(pwd)/prebuilts/clang/host/linux-x86/clang-r450784d/bin/ld &> /dev/null
