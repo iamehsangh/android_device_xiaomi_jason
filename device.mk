c55a0bc8868509ebf30d5509d80807aa653f91d6
Bruno Martins <bgcngm@gmail.com>
2022-11-01 10:39:55 +0000

    jason: Use piex shim from hardware/lineage/compat
    Change-Id: Id7cb952af127f471bd4a385a2c425ce518bb8c04
    Signed-off-by: Nanhumly <admin@warbler.cc>

--- device.mk
+++ device.mk
@@ -48,6 +48,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
+ 
+# Camera
+PRODUCT_PACKAGES += \
+    libpiex_shim
 
 # Consumerir
 BOARD_HAVE_IR := true
@@ -124,9 +128,6 @@ PRODUCT_PACKAGES += \
     libstdc++.vendor \
     mediametricsservice-aidl-cpp.vendor \
     vndk-ext
-
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libpiex.so:$(TARGET_COPY_OUT_VENDOR)/lib/libpiex-v29.so
 
 # Wifi
 PRODUCT_PACKAGES += \
