056ee29deef1316ae5bb6d8c2e71138e27dfc837
Nanhumly <admin@warbler.cc>
2023-07-22 10:04:41 +0000

    jason: Switch to stock camera hal
    Co-authored-by: jimmy <quesoo@gmail.com>
    Co-authored-by: iamehsangh <ehsan.ghasab@gmail.com>
    Co-authored-by: Arian <arian.kulmer@web.de>
    Change-Id: If1915477415cc7259fa37ac580fe8fbff763cebe
    Signed-off-by: Nanhumly <admin@warbler.cc>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,7 +31,6 @@ TARGET_KERNEL_CONFIG := jason_defconfig
 
 # Camera
 BOARD_QTI_CAMERA_32BIT_ONLY := true
-$(call project-set-path,qcom-camera,$(DEVICE_PATH)/camera)
 
 # Display
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS := 0x00002000U
